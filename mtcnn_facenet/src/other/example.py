6d60832a74d66c8a7454c01afd4f9c534110287d
zengzh28 <zengzh28@mail2.sysu.edu.cn>
2019-05-20 18:05:17 +0800

    [modify] example and path_setting. Now we can test

--- mtcnn_facenet/src/other/example.py
+++ mtcnn_facenet/src/other/example.py
@@ -13,6 +13,7 @@ from test_pkl_calculate import test_pkl_lib_add
 from PIL import Image
 import cv2
 import brightness_and_contrast
+import time
 
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -31,35 +32,52 @@ def del_file(path):
 detector = MTCNN()
 
 #lib_complete()                #complete pkl_file of face_lib when new faces add into lib
-with open('D:\\test.txt' , 'w') as data_txt :
-    data_txt.write('photo name' + '\n' + 'actual face num' + '\t' + 'detect face num' + '\n')
+with open(RESULT_FILE , 'w') as data_txt :
+    data_txt.write('photo_name' + ' ' + 'actual_fac_num' + ' ' + 'detect_face_num' + ' ' + 'detect_rate' + ' ' + 'detect_time' +  '\n')
 filename_external = os.listdir(TEST_FILE)
+detected_photo_num = 0
+total_photo_num = 0
+print_limit = 0.01
+for i in range(len(filename_external)) :    
+    total_photo_num += len(os.listdir(TEST_FILE + filename_external[i]))
+
 for i in range(len(filename_external)) :
     filename_internal = os.listdir(TEST_FILE + filename_external[i])
     for j in range(len(filename_internal)) :
         TEST_FILE_NAME = TEST_FILE + filename_external[i] + "\\" + filename_internal[j]
         #print(TEST_FILE_NAME)
         del_file(TEST_PIC_DIR)            #clear all saved test file before adding new file 
-        del_file(TEST_PKL_DIR)
+        #del_file(TEST_PKL_DIR)
         image = cv2.imread(TEST_FILE_NAME)
-        results = detector.detect_faces(image)
+        time_start=time.time()                              #timing start
+        results = detector.detect_faces(image)           #detect face
         Split_slice_detect.saveFaces(results , image)
+        time_end=time.time()                                #timing end
+        time_total = round(time_end - time_start , 3)
         detect_face_num = len(os.listdir(TEST_PIC_DIR))
 
-        with open(DATA_DIR + "\\wider_face_val_bbx_gt_new.txt" , 'rb') as face_num_file:
+        with open(FACE_NUM_LIST , 'rb') as face_num_file:
             file_context = face_num_file.read().decode('utf-8')
             photo_name = filename_external[i] + "/" + filename_internal[j]
-            print(photo_name)
-            face_index = file_context.find(photo_name)
+            face_index = file_context.find(photo_name)   #find the address of first char of photo_name
             str_face_num = ''
-            for k in range(2,6) :                        
+            for k in range(2,6) :                        #k=1 is '\n', which is not need
                 next_char = file_context[face_index + len(photo_name) + k]         
-                if next_char == '\n' or next_char == '\r' or next_char == '':
+                if next_char == '\n' :
                     break
                 str_face_num += next_char
         actual_face_num = int(str_face_num)
-        with open('D:\\test.txt' , 'a') as data_txt :
-            data_txt.write(photo_name + '\n' + str_face_num + '\t' + str(detect_face_num) + '\n')
+        detect_rate = round(detect_face_num/actual_face_num , 3)
+        with open(RESULT_FILE , 'a') as data_txt :
+            data_txt.write(photo_name + ' ' + str_face_num + ' ' + str(detect_face_num)+ ' '+ str(detect_rate) + ' ' + str(time_total)+ '\n' )
+
+        detected_photo_num += 1
+        detected_rate = detected_photo_num/total_photo_num
+        if detected_rate > print_limit :
+            print("have detected " + str(round(detected_rate , 2)))
+            print_limit += 0.01
+
+        
         
 
 
--- mtcnn_facenet/src/other/path_settings.py
+++ mtcnn_facenet/src/other/path_settings.py
@@ -12,7 +12,22 @@ DATA_DIR = os.path.dirname(DATA_DIR)
 DATA_DIR = os.path.dirname(DATA_DIR)
 DATA_DIR = os.path.join(DATA_DIR,'data')
 
-TEST_FILE = DATA_DIR + '\\face_picture\\zzzl.jpg'               #path of target file
+#D:\proj_face_recog\mtcnn_facenet\mtcnn_facenet\src\other
+#D:\proj_face_recog\mtcnn_facenet\mtcnn_facenet\src
+#D:\proj_face_recog\mtcnn_facenet\mtcnn_facenet\src
+#D:\proj_face_recog\mtcnn_facenet\mtcnn_facenet\src
+#D:\proj_face_recog\mtcnn_facenet\mtcnn_facenet\src\other
+#D:\proj_face_recog\mtcnn_facenet\mtcnn_facenet\src
+#D:\proj_face_recog\mtcnn_facenet\mtcnn_facenet
+#D:\proj_face_recog\mtcnn_facenet\mtcnn_facenet\data
+
+
+
+
+
+    
+
+TEST_FILE = DATA_DIR + '\\face_picture\\'               #path of target file
 
 TEST_PIC_DIR = DATA_DIR +'\\face_test\\jpg_file\\'   #path of the faces to solve
 
@@ -21,3 +36,7 @@ TEST_PKL_DIR = DATA_DIR + '\\face_test\\pkl_file\\'
 LIB_PIC_DIR = DATA_DIR + '\\face_lib\\jpg_file\\'      #path of the lib face picture
  
 LIB_PKL_DIR = DATA_DIR + '\\face_lib\\pkl_file\\'     #path of the lib pkl file 
+
+RESULT_FILE = DATA_DIR + '\\result.txt'
+
+FACE_NUM_LIST = DATA_DIR + "\\wider_face_val_bbx_gt_new.txt"
